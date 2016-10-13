c43a30100df93b8b29d2234748e38bc4772e33e5
Yacine Filali <yfilali@hotmail.com>
2016-10-13 14:41:29 -0700

    removed obsolete todo

--- examples/flask_pynamodb/models.py
+++ examples/flask_pynamodb/models.py
@@ -5,7 +5,6 @@ from pynamodb.models import Model
 
 
 class Department(Model):
-
     class Meta:
         table_name = 'flask_pynamodb_example_department'
         host = "http://localhost:8000"
@@ -15,7 +14,6 @@ class Department(Model):
 
 
 class Role(Model):
-
     class Meta:
         table_name = 'flask_pynamodb_example_roles'
         host = "http://localhost:8000"
@@ -25,14 +23,12 @@ class Role(Model):
 
 
 class Employee(Model):
-
     class Meta:
         table_name = 'flask_pynamodb_example_employee'
         host = "http://localhost:8000"
 
     id = UnicodeAttribute(hash_key=True)
     name = UnicodeAttribute()
-    # TODO DynamoDB doesn't have any date function so we can't set a default of now()
     hired_on = UTCDateTimeAttribute(default=datetime.now)
     department_id = UnicodeAttribute(null=False)
     role_id = UnicodeAttribute(null=False)
