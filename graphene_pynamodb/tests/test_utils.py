1c58d7e7aee09f433f0e279e923c9acb038646b5
Yacine Filali <yfilali@hotmail.com>
2016-10-18 18:49:35 -0700

    Added tests for utils

---
+++ graphene_pynamodb/tests/test_utils.py
@@ -0,0 +1,32 @@
+import pytest
+from pynamodb.attributes import UnicodeAttribute, NumberAttribute
+from pynamodb.models import Model
+
+from ..utils import get_key_name
+
+
+def test_getkeyname_should_raiseerror():
+    with pytest.raises(TypeError):
+        assert get_key_name("This is not a model") == False
+
+
+def test_getkeyname_should_workonstrings():
+    class MyModel(Model):
+        class Meta:
+            table_name = 'some-table'
+
+        notmyid = UnicodeAttribute(null=True)
+        myid = UnicodeAttribute(hash_key=True)
+
+    assert get_key_name(MyModel) == 'myid'
+
+
+def test_getkeyname_should_workonnumbers():
+    class MyModel(Model):
+        class Meta:
+            table_name = 'some-table'
+
+        notmyid = UnicodeAttribute(null=True)
+        myid = NumberAttribute(hash_key=True)
+
+    assert get_key_name(MyModel) == 'myid'
