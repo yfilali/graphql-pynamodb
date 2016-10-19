e9fdfc44e68f61400f7ad5bf3d0dd41472d9ce4c
Yacine Filali <yfilali@hotmail.com>
2016-10-18 22:43:43 -0700

    Bumped pypi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='graphene-pynamodb',
-    version='0.6',
+    version='0.6.1',
 
     description='Graphene PynamoDB integration',
     long_description=open('README.rst').read(),
