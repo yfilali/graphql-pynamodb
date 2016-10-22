193bfc2c00b201626fb4c091c0df017520ed10c2
Yacine Filali <yfilali@hotmail.com>
2016-10-21 17:45:04 -0700

    Fixed requirements for examples - New Pypi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='graphene-pynamodb',
-    version='0.6.2',
+    version='0.6.3',
 
     description='Graphene PynamoDB integration',
     long_description=open('README.rst').read(),
