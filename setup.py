c6244ef1dc9eef293b190dbc009834640ddcf87d
Yacine Filali <yfilali@hotmail.com>
2018-10-04 23:57:35 -0700

    Upgradig pynamodb dependency

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         'graphql-core < 3.0, >=2.0',
         'graphene < 3.0, >= 2.0',
         'botocore < 1.11.0, >= 1.10.0',
-        'pynamodb < 3.0.0, >=2.0.0',
+        'pynamodb >=2.0.0, <=4.0.0',
         'singledispatch>=3.4.0.3',
         'wrapt>=1.10.8'
     ],
