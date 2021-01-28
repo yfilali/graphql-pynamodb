b48ba0d58640a7286d34f9bd438d6af16dce83dc
Yacine Filali <yfilali@fb.com>
2021-01-27 18:59:08 -0800

    bumping version for dependency change on Pynamo < 5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='graphene-pynamodb',
-    version='2.3.0',
+    version='2.3.1',
 
     description='Graphene PynamoDB integration',
     long_description=open('README.rst').read(),
